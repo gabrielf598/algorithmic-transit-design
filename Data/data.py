709a55bc48cddc4e4c5692142906faa9bbe85543
Fabian Gabriel <121213635+gabrielf598@users.noreply.github.com>
2025-05-09 00:30:41 -0700

    Update data.py

--- Data/data.py
+++ Data/data.py
@@ -1,10 +1,10 @@
 # Paths to data files
 streets = "https://services.arcgis.com/ZOyb2t4B0UYuYNYH/arcgis/rest/services/Street_Network_Database_SND/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
 neighborhoods = "https://services.arcgis.com/ZOyb2t4B0UYuYNYH/arcgis/rest/services/nma_nhoods_sub/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
+development = "https://services.arcgis.com/ZOyb2t4B0UYuYNYH/arcgis/rest/services/Zoned_Development_Capacity_Layers_2016/FeatureServer/6/query?outFields=*&where=1%3D1&f=geojson"
 
 # GeoJSON files
 blocks = "./Data/blocks.geojson"
-development = "./Data/development.geojson"
 rect = "./Data/rect.geojson"
 stop_coverage = "./Data/stop_coverage.geojson"
 stops = "./Data/stops.geojson"
